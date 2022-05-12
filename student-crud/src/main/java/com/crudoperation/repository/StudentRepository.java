fa2a0139c2a5080e13f6191de0225ef5d40ed969
GirirajSingh <GirirajSingh@Giriraj_Singh>
2022-05-12 12:09:05 +0530

    project structure fix

--- student-crud/src/main/java/com/crudoperation/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crudoperation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crudoperation.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-	
-}
